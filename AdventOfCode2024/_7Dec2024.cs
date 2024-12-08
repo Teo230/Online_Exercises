15e3dc2b8bcaebf8ac62ef8be02c2ccb2822fab6
Teo <34417048+Teo230@users.noreply.github.com>
2024-12-08 15:23:03 +0000

    to finish part 2

--- AdventOfCode2024/_7Dec2024.cs
+++ AdventOfCode2024/_7Dec2024.cs
@@ -39,8 +39,32 @@ namespace Online_Exercises.AdventOfCode2024
         public static void Part2()
         {
             var textLines = File.ReadAllLines("AdventOfCode2024/7Dec2024.txt");
+            
+            long result = 0;
+            for(int i = 0; i < textLines.Length; i++)
+            {
+                var line = textLines[i];
 
-            Console.WriteLine("07/12/24 Part 2 - {0}", 0);
+                var sumResult = long.Parse(line.Split(": ")[0]);
+                var numbers = line.Split(": ")[1].Split(" ").ToList().Select(x => long.Parse(x)).ToList();
+
+                var isValid = IsValid2(sumResult, numbers);
+                if(isValid) result += sumResult;
+            }
+
+            Console.WriteLine("07/12/24 Part 2 - {0}", result);
+        }
+
+        private static bool IsValid2(long sumResult, List<long> numbers)
+        {
+            if(numbers.Count == 1) return sumResult == numbers[0];
+            var currentNumber = numbers.Last();
+            if(sumResult % currentNumber == 0 && IsValid(sumResult / currentNumber, numbers.SkipLast(1).ToList())) return true;
+            if(sumResult > currentNumber && IsValid(sumResult - currentNumber, numbers.SkipLast(1).ToList())) return true;
+            
+            //...
+
+            return false;
         }
     }
 }
