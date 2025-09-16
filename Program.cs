7b68a039a3aa4bcd7b26999c26ff0f5682c5d16e
Teo230 <mfantin230@gmail.com>
2025-09-16 19:46:54 +0200

    lock sample

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using Online_Exercises.AdventOfCode2015;
 using Online_Exercises.AdventOfCode2022;
 using Online_Exercises.AdventOfCode2024;
+using Online_Exercises.Misc;
 using Online_Exercises.TopInterview150;
 
-/// Top 150 Interview
+#region Top 150 Interview
 
 //_88_MergeSortedArray.Test();
 //_108_SortedArrayToTree.Test();
 //_125_ValidPalindrome.Test();
 //_226_InvertBinaryTree.Test();
 
-/// Advent of 2015
+#endregion
+
+#region Advent of 2015
 
 //_1Dec2015.Part1();
 //_1Dec2015.Part2();
 
-/// Advent of 2022
+#endregion
+
+#region Advent of 2022
 
 //_7Dec2022.Part1();
 //_7Dec2022.Part2();
+
+#endregion
 
 #region Advent of 2024
 
@@ -49,8 +56,14 @@ using Online_Exercises.TopInterview150;
 // _9Dec2024.Part1();
 // _9Dec2024.Part2();
 
-_10Dec2024.Part1();
-_10Dec2024.Part2();
+//_10Dec2024.Part1();
+//_10Dec2024.Part2();
+
+#endregion
+
+#region Misc
+
+LockSample.Main();
 
 #endregion
 
